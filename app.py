e89ea528d040d98900c6c1dd55267729b2b5b050
Nicholas Anderson <nanderson993@gmail.com>
2020-04-29 16:33:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:hellodolly@localhost:5433/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:*********@localhost:5433/todoapp'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
