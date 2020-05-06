e9227b45950571dadcdb48fe24860fcc7c3eb0c8
Nicholas Anderson <nanderson993@gmail.com>
2020-05-06 16:03:50 -0500

    add app routers, relationships

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sys
+from jinja2 import Template, Environment
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:********@localhost:5433/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:hellodolly@localhost:5433/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
-
 
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable=False, default=False)
+  completed = db.Column(db.Boolean, nullable=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-@app.route('/todos/create', method=['POST'])
+
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
   error = False
   body = {}
   try:
-    description = request.form.get_json()['description']
-    todo = Todo(description=description)
+    description = request.get_json()['description']
+    todo = Todo(description=description, completed=False)
     db.session.add(todo)
     db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
     body['description'] = todo.description
   except:
     error = True
@@ -39,6 +43,20 @@ def create_todo():
   else:
     return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', todos=Todo.query.order_by('id').all())
