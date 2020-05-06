0692986e9fff3effbd761f6f6e9611dcd7bfaf4c
Nicholas Anderson <nanderson993@gmail.com>
2020-05-06 15:35:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:hellodolly@localhost:5433/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nanderson992:********@localhost:5433/todoapp'
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
